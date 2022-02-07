ead5d4f9476b410152973a4e7d710a1587d983a7
wssds Sn Gk <ssean.gerigk@gmx.net>
2022-02-07 08:59:30 +0100

    firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'simple-crm-9319b',
+    appId: '1:8793361268:web:80f08153e14052a82d3a21',
+    storageBucket: 'simple-crm-9319b.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyDj3GpE5F8DNTq1OL3iYJpqnWhUaK69OtM',
+    authDomain: 'simple-crm-9319b.firebaseapp.com',
+    messagingSenderId: '8793361268',
+  },
   production: true
 };
